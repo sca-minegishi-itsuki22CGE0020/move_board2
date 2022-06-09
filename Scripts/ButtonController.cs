263f7ae488eb00f7b16560b540a0020ed702b633
kazuki-PC\kazuki <ittsu.1106@gmail.com>
2022-06-10 00:31:41 +0900

    最終調整

---
+++ Scripts/ButtonController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonController : MonoBehaviour
+{
+    public void ChengeScene()
+    { 
+        SceneManager.LoadScene("move_board");
+    }
+}
+
+ 
---
+++ Scripts/move_board.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move_board : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float sense = 0.05f; //è∞ÇÃìÆÇ≠ë¨Ç≥
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+
+        transform.Rotate(moveHorizontal * sense, 0, moveVertical * sense);
+    }
+}
